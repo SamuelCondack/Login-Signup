27dec161981c998706a5883a58fe570df118f535
Samuel Condack <Samuelcondackxz@gmail.com>
2023-09-08 19:21:33 -0300

    fix: Fix The JS Redirection Page Click Function

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ const password = document.getElementById('password')
 const submit = document.getElementById('submit').addEventListener('click', ()=>{
     if(window.sessionStorage.getItem(user.value) === password.value){
         document.getElementById('loginError').classList.remove("loginError")
-        window.location.href = "http://127.0.0.1:5500/logged/logged.html"
+        window.location.href = "samuelcondacklogin-signup.netlify.app/logged/logged.html"
     } else {
         document.getElementById('loginError').classList.add("loginError")
         setTimeout(()=>{document.getElementById('loginError').classList.remove("loginError")}, 3000)
